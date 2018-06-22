3029c2278d3dfcbe8a882578462b152aa0bdcf0d
sydinh <sydinh.dev@gmail.com>
2018-06-22 11:25:11 +0700

    Add more JS :fire:

---
+++ js/built-in.js
@@ -0,0 +1,9 @@
+const root = document.getElementById('root');
+
+class HelloPWA extends React.Component {
+  render() {
+    return <h1>Hello Progressive Web App</h1>;
+  }
+}
+
+ReactDOM.render(<HelloPWA />, root);
