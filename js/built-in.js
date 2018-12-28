565ce5f2af84c539ed8a7f2a833d1475914c790c
sydinh <sydinh.dev@gmail.com>
2018-12-28 14:54:05 +0700

    Refactor

--- js/built-in.js
+++
@@ -1,5 +0,0 @@
-const root = document.getElementById('hello-pwa');
-
-const HelloPWA = () => <h1>Hello Progressive Web App</h1>;
-
-ReactDOM.render(<HelloPWA />, root);
--- service-worker.js
+++ service-worker.js
@@ -1,63 +1,63 @@
-const CACHE_NAME = 'my-first-pwa-cache-v1';
+const CACHE_NAME = "my-first-pwa-cache-v1";
 const urlsToCache = [
-  './',
-  './css/built-in.css',
-  './css/built-in-offline-page.css',
-  './js/babel.js',
-  './js/react.development.js',
-  './js/react-dom.development.js',
-  './js/built-in.js',
+  "./",
+  "./css/built-in.css",
+  "./css/built-in-offline-page.css",
+  "./js/babel.js",
+  "./js/react.development.js",
+  "./js/react-dom.development.js",
+  "./js/built-in.js"
 ];
 
 // Install service worker
-self.addEventListener('install', (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(urlsToCache);
-    }),
+    })
   );
 });
 
 // Update service worker
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", event => {
   const cacheWhitelist = [
-    'my-first-pwa-cache-v1-foo',
-    'my-first-pwa-cache-v1-bar',
+    "my-first-pwa-cache-v1-foo",
+    "my-first-pwa-cache-v1-bar"
   ];
 
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
+        cacheNames.map(cacheName => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
-        }),
+        })
       );
-    }),
+    })
   );
 });
 
 // Cache and return requests
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       if (response) {
         return response;
       }
       const fetchRequest = event.request.clone();
-      return fetch(fetchRequest).then((response) => {
-        if (!response || response.status !== 200 || response.type !== 'basic') {
+      return fetch(fetchRequest).then(response => {
+        if (!response || response.status !== 200 || response.type !== "basic") {
           return response;
         }
         const responseToCache = response.clone();
 
-        caches.open(CACHE_NAME).then((cache) => {
+        caches.open(CACHE_NAME).then(cache => {
           cache.put(event.request, responseToCache);
         });
 
         return response;
       });
-    }),
+    })
   );
 });
